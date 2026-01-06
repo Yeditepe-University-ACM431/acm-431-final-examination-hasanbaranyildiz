1cd74dc92f8b886ae481d142b1044f9af20f715e
hasanbaranyildiz <150271813+hasanbaranyildiz@users.noreply.github.com>
2026-01-06 15:04:16 +0300

    Update TaskRepository.kt

--- app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
+++ app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
@@ -2,12 +2,20 @@ package com.yeditepe.finalexam.repository
 
 import com.yeditepe.finalexam.api.TaskApi
 import com.yeditepe.finalexam.model.Task
-
 class TaskRepository(private val api: TaskApi) {
 
-    suspend fun fetchTasks(): List<Task> {
-        // TODO 1: Call API
-        // TODO 2: Convert TaskDto list to Task list
-        return TODO("Provide the return value")
+        suspend fun fetchTasks(): List<Task> {
+
+
+            val taskDtoList = api.getTasks()
+
+
+            return taskDtoList.map { dto ->
+                Task(
+                    title = dto.title,
+                    description = dto.description,
+                    completed = dto.completed
+                )
+            }
+        }
     }
-}
